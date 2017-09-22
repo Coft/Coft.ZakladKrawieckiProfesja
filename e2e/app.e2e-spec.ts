51e52bf087bf8d0cb6bb11701b1e1f7ce1b0bd07
unknown <coft@o2.pl>
2017-09-22 18:29:28 +0200

    init

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('coft.zaklad-krawiecki-profesja App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
